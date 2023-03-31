011f3bf6898963d9f4bf04d89e1bed3c90aeeedc
Prashant Sharma <prashant.rmg@gmail.com>
2023-03-30 21:20:19 -0500

    dsfsdf

--- myfirstazureapi/Controllers/WeatherForecastController.cs
+++ myfirstazureapi/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chill", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chill", "Cool", "Mild", "Warmer", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
